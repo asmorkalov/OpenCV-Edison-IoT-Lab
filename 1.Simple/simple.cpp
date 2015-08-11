b0ec1ae6715b53b7d604bc34d93d86013ea0897a
Alexander Smorkalov <alexander.smorkalov@itseez.com>
2015-08-12 00:12:48 +0300

    Added simple image processing sample with OpenCV.

---
+++ 1.Simple/simple.cpp
@@ -0,0 +1,15 @@
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+    Mat img, gray, edges;
+    img = imread(argv[1], 1);
+    cvtColor(img, gray, COLOR_BGR2GRAY);
+    GaussianBlur(gray, gray, Size(7, 7), 1.5);
+    Canny(gray, edges, 0, 50);
+    imwrite("result.png", edges);
+    waitKey();
+    return 0;
+}
